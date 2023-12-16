8b01e7c0796066aa9dfca0af05ab585bccc21c4b
Herzanich Georgii <152099923+Georgiy-Herzanich@users.noreply.github.com>
2023-12-17 00:02:59 +0200

    push

---
+++ PW2/src/main/java/com/example/georgij/pw2/AppConfig.java
@@ -0,0 +1,10 @@
+package com.example.georgij.pw2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.example.ihorovich.pw2")
+public class AppConfig {
+
+}
---
+++ PW2/src/main/java/com/example/georgij/pw2/Cart.java
@@ -0,0 +1,31 @@
+package com.example.georgij.pw2;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Cart {
+  public ProductRepository productRepository;
+  public Map<Product, Integer> products;
+
+  public Cart(ProductRepository productRepository){
+    this.productRepository = productRepository;
+    products = new HashMap<>();
+  }
+
+  public Map<Product, Integer>
+
+  public void addItem(int id){\
+
+
+    products.put(productRepository.findById(id));
+  }
+
+  public void deleteItem(int id){
+    productList.remove(id);
+  }
+
+}
